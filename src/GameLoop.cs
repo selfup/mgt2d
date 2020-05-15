eb0afeecd34e3a58171fe3f9c137ffb3f4e6e426
selfup <boudinot.regis@yahoo.com>
2020-05-15 10:34:35 -0500

    formatting - launch/watch scripts auto gen

--- src/GameLoop.cs
+++ src/GameLoop.cs
@@ -1,77 +1,76 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace mgt2d {
-  public class GameLoop : Game {
-    private GraphicsDeviceManager _graphics;
-    private SpriteBatch _spriteBatch;
-    private Texture2D _player;
-    private Rectangle character;
-    private PlayerControls playerControls;
+    public class GameLoop : Game {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _player;
+        private Rectangle character;
+        private PlayerControls playerControls;
 
-    public GameLoop() {
-      _graphics = new GraphicsDeviceManager(this);
+        public GameLoop() {
+            _graphics = new GraphicsDeviceManager(this);
 
-      playerControls = new PlayerControls(this);
-      Content.RootDirectory = "Content";
-      IsMouseVisible = true;
+            playerControls = new PlayerControls(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize() {
+            var displayWidth = _graphics.GraphicsDevice.DisplayMode.Width;
+            var displayHeight = _graphics.GraphicsDevice.DisplayMode.Height;
+
+            _graphics.PreferredBackBufferWidth = displayWidth;
+            _graphics.PreferredBackBufferHeight = displayHeight;
+            _graphics.IsFullScreen = false;
+            _graphics.ApplyChanges();
+
+            Window.AllowUserResizing = true;
+            Window.IsBorderless = true;
+
+            var deviceWidth = GraphicsDevice.Viewport.Bounds.Width;
+            var deviceHeight = GraphicsDevice.Viewport.Bounds.Height;
+
+            var centerX = deviceWidth / 2;
+            var centerY = deviceHeight / 2;
+
+            int spriteSize;
+
+            if (deviceWidth > 1919) {
+                spriteSize = 64;
+            } else if (deviceWidth > 1279) {
+                spriteSize = 32;
+            } else {
+                spriteSize = 16;
+            }
+
+            character = new Rectangle(centerX, centerY, spriteSize, spriteSize);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            LoadSprites sprites = new LoadSprites(GraphicsDevice);
+            _player = sprites.player;
+        }
+
+        protected override void Update(GameTime gameTime) {
+            playerControls.handleGameInput(character);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Transparent);
+            GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_player, character, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
     }
-
-    protected override void Initialize() {
-      var displayWidth = _graphics.GraphicsDevice.DisplayMode.Width;
-      var displayHeight = _graphics.GraphicsDevice.DisplayMode.Height;
-
-      _graphics.PreferredBackBufferWidth = displayWidth;
-      _graphics.PreferredBackBufferHeight = displayHeight;
-      _graphics.IsFullScreen = false;
-      _graphics.ApplyChanges();
-
-      Window.AllowUserResizing = true;
-      Window.IsBorderless = true;
-
-      var deviceWidth = GraphicsDevice.Viewport.Bounds.Width;
-      var deviceHeight = GraphicsDevice.Viewport.Bounds.Height;
-
-      var centerX = deviceWidth / 2;
-      var centerY = deviceHeight / 2;
-
-      int spriteSize;
-
-      if (deviceWidth > 1919) {
-        spriteSize = 64;
-      } else if (deviceWidth > 1279) {
-        spriteSize = 32;
-      } else {
-        spriteSize = 16;
-      }
-
-      character = new Rectangle(centerX, centerY, spriteSize, spriteSize);
-
-      base.Initialize();
-    }
-
-    protected override void LoadContent() {
-      _spriteBatch = new SpriteBatch(GraphicsDevice);
-      LoadSprites sprites = new LoadSprites(GraphicsDevice);
-      _player = sprites.player;
-    }
-
-    protected override void Update(GameTime gameTime) {
-      playerControls.handleGameInput(character);
-
-      base.Update(gameTime);
-    }
-
-    protected override void Draw(GameTime gameTime) {
-      GraphicsDevice.Clear(Color.Transparent);
-      GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
-
-      _spriteBatch.Begin();
-      _spriteBatch.Draw(_player, character, Color.White);
-      _spriteBatch.End();
-
-      base.Draw(gameTime);
-    }
-  }
 }
--- src/PlayerControls.cs
+++ src/PlayerControls.cs
@@ -1,79 +1,75 @@
-using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace mgt2d {
-  class PlayerControls {
-    private Game game;
+    class PlayerControls {
+        private Game game;
 
-    public PlayerControls(Game scopedGame) {
-      game = scopedGame;
+        public PlayerControls(Game scopedGame) {
+            game = scopedGame;
+        }
+
+        public void handleGameInput(Rectangle character) {
+            var newX = (character.X * 1);
+            var newY = (character.Y * 1);
+
+            var keyboardState = Keyboard.GetState();
+            var gamePadState = GamePad.GetState(PlayerIndex.One);
+            var gamePadButtons = gamePadState.Buttons;
+            var gamePadSticks = gamePadState.ThumbSticks;
+
+            // "-" button on switch
+            var sBack = gamePadButtons.Back;
+
+            // switch specific
+            var sA = gamePadButtons.B;
+            var sB = gamePadButtons.A;
+            var sX = gamePadButtons.Y;
+            var sY = gamePadButtons.X;
+
+            // keyboard specific
+            var kBack = Keys.Escape;
+            var kW = Keys.W;
+            var kS = Keys.S;
+            var kD = Keys.D;
+            var kA = Keys.A;
+
+            if (keyboardState.IsKeyDown(kW)) {
+                newY += 1;
+            } else if (keyboardState.IsKeyDown(kS)) {
+                newY -= 1;
+            } else if (keyboardState.IsKeyDown(kD)) {
+                newX += 1;
+            } else if (keyboardState.IsKeyDown(kA)) {
+                newX -= 1;
+            }
+
+            if (sBack == ButtonState.Pressed || keyboardState.IsKeyDown(kBack)) {
+                game.Exit();
+            }
+
+            var isController = false;
+
+            if (sA == ButtonState.Pressed) {
+                isController = true;
+            }
+            if (sB == ButtonState.Pressed) {
+                isController = true;
+            }
+            if (sX == ButtonState.Pressed) {
+                isController = true;
+            }
+            if (sY == ButtonState.Pressed) {
+                isController = true;
+            }
+
+            if (isController) {
+                newX = (int)gamePadSticks.Left.X;
+                newY = (int)gamePadSticks.Left.Y;
+            }
+
+            character.X = character.X + (newX * 10);
+            character.Y = character.Y - (newY * 10);
+        }
     }
-
-    public void handleGameInput(Rectangle character) {
-      var newX = (character.X * 1);
-      var newY = (character.Y * 1);
-
-      var keyboardState = Keyboard.GetState();
-      var gamePadState = GamePad.GetState(PlayerIndex.One);
-      var gamePadButtons = gamePadState.Buttons;
-      var gamePadSticks = gamePadState.ThumbSticks;
-
-      // "-" button on switch
-      var sBack = gamePadButtons.Back;
-
-      // switch specific
-      var sA = gamePadButtons.B;
-      var sB = gamePadButtons.A;
-      var sX = gamePadButtons.Y;
-      var sY = gamePadButtons.X;
-
-      // keyboard specific
-      var kBack = Keys.Escape;
-      var kW = Keys.W;
-      var kS = Keys.S;
-      var kD = Keys.D;
-      var kA = Keys.A;
-
-      if (keyboardState.IsKeyDown(kW)) {
-        newY += 1;
-      }
-      if (keyboardState.IsKeyDown(kS)) {
-        newY -= 1;
-      }
-      if (keyboardState.IsKeyDown(kD)) {
-        newX += 1;
-      }
-      if (keyboardState.IsKeyDown(kA)) {
-        newX -= 1;
-      }
-
-      if (sBack == ButtonState.Pressed || keyboardState.IsKeyDown(kBack)) {
-        game.Exit();
-      }
-
-      var isController = false;
-
-      if (sA == ButtonState.Pressed) {
-        isController = true;
-      }
-      if (sB == ButtonState.Pressed) {
-        isController = true;
-      }
-      if (sX == ButtonState.Pressed) {
-        isController = true;
-      }
-      if (sY == ButtonState.Pressed) {
-        isController = true;
-      }
-
-      if (isController) {
-        newX = (int)gamePadSticks.Left.X;
-        newY = (int)gamePadSticks.Left.Y;
-      }
-
-      character.X = character.X + (newX * 10);
-      character.Y = character.Y - (newY * 10);
-    }
-  }
 }
