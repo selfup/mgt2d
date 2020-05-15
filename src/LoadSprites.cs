001ba575d908d7e48f2e515eb19281a73d92a19d
Regis <boudinot.regis@yahoo.com>
2020-05-15 12:17:05 -0500

    indentation settings via omnisharp

--- src/LoadSprites.cs
+++ src/LoadSprites.cs
@@ -2,16 +2,13 @@ using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace mgt2d
-{
-  public class LoadSprites : Game
-  {
-    public Texture2D player;
-    public LoadSprites(GraphicsDevice graphicsDevice)
-    {
-      string playerPath = "Content/sprites/player.png";
-      using (Stream playerFile = File.OpenRead(playerPath))
-        player = Texture2D.FromStream(graphicsDevice, playerFile);
+namespace mgt2d {
+    public class LoadSprites : Game {
+        public Texture2D player;
+        public LoadSprites(GraphicsDevice graphicsDevice) {
+            string playerPath = "Content/sprites/player.png";
+            using (Stream playerFile = File.OpenRead(playerPath))
+                player = Texture2D.FromStream(graphicsDevice, playerFile);
+        }
     }
-  }
 }
